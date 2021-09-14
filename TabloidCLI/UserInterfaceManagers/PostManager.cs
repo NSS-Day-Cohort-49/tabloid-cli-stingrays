df2558db48a0748ce46f32502bfca417aa855441
Sam Carbine <scarbine@me.com>
2021-09-14 17:16:47 -0500

    Added affordance for user to add a new post

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -1,10 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class Class1
+    class PostManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private AuthorRepository _authorRepository;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _authorRepository = new AuthorRepository(connectionString);
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) List Posts");
+            Console.WriteLine(" 2) Add Post");
+           
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+
+                case "2":
+                    AddPost();
+                    return this;
+
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        public void List()
+        {
+            List<Post> posts = _postRepository.GetAll();
+            foreach (Post p in posts)
+            {
+                Console.WriteLine($"{p.Id} ) {p.Title} {p.Url} {p.PublishDateTime} ");
+            }
+        }
+
+        public void AddPost()
+        {
+            List<Author> authors = _authorRepository.GetAll();
+            List<Blog> blogs = _blogRepository.GetAll();
+
+            Post post = new Post();
+
+            Console.WriteLine("Please enter the title of your post.");
+            post.Title = Console.ReadLine();
+            Console.WriteLine("Please enter the url of your post");
+            post.Url = Console.ReadLine();
+            post.PublishDateTime = new DateTime();
+
+            Console.WriteLine("Please select an Author for this post");
+            foreach (Author a in authors)
+            {
+                Console.WriteLine($"{a.Id} ) {a.FullName}");
+            }
+            Console.Write("> ");
+            int authorId = int.Parse(Console.ReadLine());
+
+            post.Author = _authorRepository.Get(authorId);
+
+
+
+            
+        }
+
+
     }
 }
