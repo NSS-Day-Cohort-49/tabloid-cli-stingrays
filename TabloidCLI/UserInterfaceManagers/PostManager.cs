45c1c389bd7f961cd73840330451e4b3070b3b2d
ambarnett <andy.barnett09@gmail.com>
2021-09-14 15:43:15 -0500

    working on getting post structured

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    public class PostManager : IUserInterfaceManager 
+    public class PostManager : IUserInterfaceManager
     {
         private readonly IUserInterfaceManager _parentUI;
         private PostRepository _postRepository;
         private string _connectionString;
+
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+        }
     }
 }
