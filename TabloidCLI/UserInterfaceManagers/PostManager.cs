4f45d6105c56cb844f292ff9f8d16e7e1669a0b2
ambarnett <andy.barnett09@gmail.com>
2021-09-14 15:04:58 -0500

    beginning work on posts

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class PostManager : IUserInterfaceManager 
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private string _connectionString;
+    }
+}
