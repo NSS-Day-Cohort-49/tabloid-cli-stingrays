bddeed8aa43f8292c04208eeda7f1c8b604e1553
Kipp Minton <luciankippminton@gmail.com>
2021-09-15 21:50:20 -0500

    menu option for background color selection

---
+++ TabloidCLI/UserInterfaceManagers/BackgroundColorManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class BackgroundColorManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+
+        public BackgroundColorManager(IUserInterfaceManager parentUI)
+        {
+            _parentUI = parentUI;
+        }
+
+        public ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Select your background color");
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) {colors[i]}");
+            }
+            Console.WriteLine("0) Exit");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "2":
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "3":
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "4":
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "5":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "6":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "7":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "8":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
+                    return this;
+                case "9":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "10":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "11":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "12":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Clear();
+                    return this;
+                case "13":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "14":
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    return this;
+                case "15":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.Clear();
+                    return this;
+                case "16":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+
+        }
+    }
+}
+
+
+//       The background color is DarkGray.
+//       The background color is Blue.
+//       The background color is Green.
+//       The background color is Cyan.
+//       The background color is Red.
+//       The background color is Magenta.
+//       The background color is Yellow.
