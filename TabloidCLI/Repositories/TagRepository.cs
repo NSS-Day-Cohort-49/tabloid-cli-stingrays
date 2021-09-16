ba2669ad000544da5bb2bbce0a82ee42801ec860
Sam Carbine <scarbine@me.com>
2021-09-16 10:09:52 -0500

    Edit and Remove tags

--- TabloidCLI/Repositories/TagRepository.cs
+++ TabloidCLI/Repositories/TagRepository.cs
@@ -76,7 +76,17 @@ namespace TabloidCLI
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Tag WHERE Id = @id; DELETE FROM PostTag WHERE TagId = @id; DELETE FROM AuthorTag WHERE TagId = @id; DELETE FROM BlogTag WHERE TagId = @id;" ;
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public SearchResults<Author> SearchAuthors(string tagName)
